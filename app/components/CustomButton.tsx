cddba11da5e6fbe1388bd8b29e5ee104d697ad86
Peter Omotosho <omotoshopeter737@gmail.com>
2024-12-19 20:03:54 +0100

    Format CustomButton Text style

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -1,10 +1,10 @@
 import type { Button } from "@/types"
 
-export default function Button({text,styles,event}:Button) {
+export default function Button({ text, styles, event }: Button) {
     return (
-        <button 
-        className={styles}
-        onClick={event}
+        <button
+            className={styles}
+            onClick={event}
         >
             {text}
         </button>
